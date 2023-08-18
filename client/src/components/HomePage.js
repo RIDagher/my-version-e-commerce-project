1ccc09925967f28175251ca5cb5b26d78c290e16
valeria bocanegra <vboca660@gmail.com>
2023-08-18 15:14:54 -0400

    images

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,43 +1,44 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 function HomePage() {
+  const [products, setProducts] = useState([]);
 
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-        // TODO: GET all flight numbers
-        const fetchProducts = async () => {
-            try {
-              const response = await fetch('/getProducts');
-              if (response.ok) {
-                const data = await response.json();
-                setProducts(data.data);
-              } else {
-                console.error('Failed to fetch products]:', response.statusText);
-              }
-            } catch (error) {
-              console.error('Error fetching products:', error);
-            }
-          }      
-          fetchProducts();
-          
-    }, []);
+  useEffect(() => {
+    // TODO: GET all flight numbers
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("/getProducts");
+        if (response.ok) {
+          const data = await response.json();
+          setProducts(data.data);
+        } else {
+          console.error("Failed to fetch products]:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+    fetchProducts();
+  }, []);
 
   return (
     <Wrapper>
-        {products.map((element) => {
-            return <p key={element._id}>{element.name}</p>
-        })}
+      {products.map((element) => {
+        return (
+          <>
+            <p key={element._id}>{element.name}</p>
+            <img src={element.imageSrc} />
+          </>
+        );
+      })}
     </Wrapper>
-  )
-
+  );
 }
 
-const Wrapper = styled.div` 
-    display: flex;
-    flex-direction: column;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
-
 
 export default HomePage;
